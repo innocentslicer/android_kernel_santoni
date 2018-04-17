94c2b4a473901fe21af0cb54cc93bb4a16eb407c
Ryan Andri <ryanandri@linuxmail.org>
2018-04-17 08:12:55 +0700

    SQUASH: clean up uninitilized variables in tree
    clean up this:
    arch/arm64/mm/mmap.c
    drivers/clk/msm/clock-cpu-8939.c
    drivers/clk/msm/clock-osm.c
    drivers/devfreq/simple_gpu_algorithm.c
    drivers/input/touchscreen/ft5336/ft5x06_720p.c
    drivers/regulator/cpr-regulator.c
    drivers/soundwire/swr-wcd-ctrl.c
    drivers/staging/android/lowmemorykiller.c
    drivers/usb/gadget/function/u_ether.c
    drivers/video/msm/mdss/mdss_dsi.c
    mm/zcache.c
    net/rmnet_data/rmnet_data_vnd.c
    sound/soc/codecs/msm8x16-wcd.c
    sound/soc/codecs/wcd-mbhc-v2.c
    sound/soc/codecs/wcd9335.c
    
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- sound/soc/codecs/msm8x16-wcd.c
+++ sound/soc/codecs/msm8x16-wcd.c
@@ -3681,7 +3681,9 @@ static int msm8x16_wcd_codec_set_iir_gain(struct snd_soc_dapm_widget *w,
 		struct snd_kcontrol *kcontrol, int event)
 {
 	struct snd_soc_codec *codec = w->codec;
-	int value = 0, reg;
+	int value = 0;
+	/* initialized to default 0x00 */
+	int reg = MSM8X16_WCD_A_CDC_IIR1_GAIN_B1_CTL__POR;
 
 	switch (event) {
 	case SND_SOC_DAPM_POST_PMU:
@@ -6331,7 +6333,7 @@ static int msm8x16_wcd_spmi_probe(struct spmi_device *spmi)
 {
 	int ret = 0;
 	struct msm8x16_wcd *msm8x16 = NULL;
-	struct msm8x16_wcd_pdata *pdata;
+	struct msm8x16_wcd_pdata *pdata = NULL;
 	struct resource *wcd_resource;
 	int adsp_state;
 	static int spmi_dev_registered_cnt;
